8df2caa18a705bd68f6e6ed29243547382bb733d
Nico Huettmann <huttmann.nico@gmail.com>
2022-02-02 12:18:43 +0100

    Add _Getting_started file

---
+++ _Getting_started.R
@@ -0,0 +1,22 @@
+
+# 
+# Getting started
+# 
+# 
+
+
+# ---- Load packages ---- 
+library(pOmics)
+
+
+
+
+# ---- Set up folder structure (only do this once) ---- 
+setup_folders()
+
+
+# ---- Download template scripts and copy to clipboard ---- 
+copy_template()
+
+download_template()
+
